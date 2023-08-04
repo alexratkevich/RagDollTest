63803445deacdb23d079d13ee3c285baeba82c8d
alexratkevich <alexratkevich@gmail.com>
2023-08-04 09:56:13 +0300

    Some update

--- Assets/Scripts/StickmanController.cs
+++ Assets/Scripts/StickmanController.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Threading.Tasks;
 
 [RequireComponent(typeof(Animator))]
 public class StickmanController : MonoBehaviour
@@ -9,6 +10,8 @@ public class StickmanController : MonoBehaviour
     private Vector3 _defaultPosition;
     private Quaternion _defaultRotation;
     private Rigidbody[] _allRigidBodies;
+    private bool _isRagDoll = false;
+    private Vector3 _deltaStickPosition;
 
     public int kickForce => _kickForce;
     
@@ -24,10 +27,13 @@ public class StickmanController : MonoBehaviour
             rb.isKinematic = true;
             rb.gameObject.AddComponent<KickController>().Initialize(this);
         }
+
+        _deltaStickPosition = transform.position - _allRigidBodies[0].transform.position;        
     }
 
     public void SetDefault()
     {
+        _isRagDoll = false;
         transform.SetPositionAndRotation(_defaultPosition, _defaultRotation);        
         _animator.enabled = true;
 
@@ -35,11 +41,43 @@ public class StickmanController : MonoBehaviour
             rb.isKinematic = true;
     }
 
-    public void SetRagDoll()
+    public void SetAnimated()
     {
+        _isRagDoll = false;
+        
+        foreach (var rb in _allRigidBodies)
+            rb.isKinematic = true;
+
+        Vector3 newPos = _allRigidBodies[0].transform.position + _deltaStickPosition;
+        transform.position = new Vector3(newPos.x, 0, newPos.z);
+
+        _animator.enabled = true;
+    }
+
+    public async void SetRagDoll()
+    {        
         _animator.enabled = false;
 
         foreach (var rb in _allRigidBodies)
-            rb.isKinematic = false;        
+            rb.isKinematic = false;
+        // so so
+        await Task.Delay(1000);
+
+        _isRagDoll = true;
+    }
+
+    private void FixedUpdate()
+    {
+        // so so
+        if (_isRagDoll)
+        {
+            foreach (var rb in _allRigidBodies)
+            {                
+                if (rb.velocity.sqrMagnitude > 0.1f)
+                    return;
+            }
+
+            SetAnimated();
+        }
     }
 }
